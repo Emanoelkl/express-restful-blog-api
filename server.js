8664260c7a5a7fdf3bceb9598c48ca56ddbf91ff
Emanoelkl <emanoel.er@hotmail.com>
2025-06-26 04:31:03 -0300

    feat: implement user registration and login with JWT

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`server running in PORT: ${process.env.PORT}`);
